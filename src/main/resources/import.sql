328c5d6a46613d13ab9332e5eb5622dd1057c88d
renatocamp <renatocontato@hotmail.com>
2025-01-18 12:56:08 -0300

    Definindo configurações dos profiles

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO tb_cliente (id_cliente, nome_cliente, senha_cliente, cpf_cliente, email_cliente, telefone_cliente) 
+VALUES (1, 'Cliente Teste', '12345', '12345678900', 'email@email.com', '19789456123');
